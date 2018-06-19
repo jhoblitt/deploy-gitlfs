916d926176cbbf354b719343a08c1b44a1f639aa
Joshua Hoblitt <josh@hoblitt.com>
2018-06-19 11:17:45 -0700

    rm basecode() -- no longer needed with kubernetes provider

--- tf/k8s.tf
+++ tf/k8s.tf
@@ -2,7 +2,7 @@ provider "kubernetes" {
   version = "~> 1.1"
 
   host                   = "${google_container_cluster.gitlfs.endpoint}"
-  client_certificate     = "${base64decode(google_container_cluster.gitlfs.master_auth.0.client_certificate)}"
-  client_key             = "${base64decode(google_container_cluster.gitlfs.master_auth.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(google_container_cluster.gitlfs.master_auth.0.cluster_ca_certificate)}"
+  client_certificate     = "${google_container_cluster.gitlfs.master_auth.0.client_certificate}"
+  client_key             = "${google_container_cluster.gitlfs.master_auth.0.client_key}"
+  cluster_ca_certificate = "${google_container_cluster.gitlfs.master_auth.0.cluster_ca_certificate}"
 }
