beced08db5cc66e9737e9ded544ecb7b78852af8
Joshua Hoblitt <josh@hoblitt.com>
2018-07-10 07:06:48 -0700

    update to 2018 *.lsst.codes tls cert

--- tf/ssl-proxy-secrets.tf
+++ tf/ssl-proxy-secrets.tf
@@ -5,8 +5,8 @@ resource "kubernetes_secret" "ssl_proxy" {
   }
 
   data {
-    proxycert = "${file("${path.module}/lsst-certs/lsst.codes/2017/lsst.codes_chain.pem")}"
-    proxykey  = "${file("${path.module}/lsst-certs/lsst.codes/2017/lsst.codes.key")}"
+    proxycert = "${file("${path.module}/lsst-certs/lsst.codes/2018/lsst.codes_chain.pem")}"
+    proxykey  = "${file("${path.module}/lsst-certs/lsst.codes/2018/lsst.codes.key")}"
     dhparam   = "${file("${path.module}/dhparam.pem")}"
   }
 }
