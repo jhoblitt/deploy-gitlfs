d9ba61a745367c5cb4f8f878f50a53a8b9998aac
Joshua Hoblitt <josh@hoblitt.com>
2018-12-27 15:51:57 -0700

    set google/template provider version constraints

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,11 @@
+provider "template" {
+  version = "~> 1.0"
+}
+
+provider "google" {
+  version = "~> 1.20"
+}
+
 module "gke" {
   source             = "git::https://github.com/lsst-sqre/terraform-gke-std.git//?ref=master"
   name               = "${local.gke_cluster_name}"
