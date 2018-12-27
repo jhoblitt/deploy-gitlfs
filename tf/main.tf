befc3078acfa8361f9a25c93410d79fb3f242b99
Joshua Hoblitt <josh@hoblitt.com>
2018-12-27 14:29:50 -0700

    update kubernetes provider config to work with rbac

--- tf/main.tf
+++ tf/main.tf
@@ -8,11 +8,11 @@ module "gke" {
 }
 
 provider "kubernetes" {
-  version = "~> 1.1"
+  version = "~> 1.4"
+
+  load_config_file = true
 
   host                   = "${module.gke.host}"
-  client_certificate     = "${base64decode(module.gke.client_certificate)}"
-  client_key             = "${base64decode(module.gke.client_key)}"
   cluster_ca_certificate = "${base64decode(module.gke.cluster_ca_certificate)}"
 }
 
