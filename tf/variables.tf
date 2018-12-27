dd0c470a5e68bf31fec9793a35a3ebb0305fb09c
Joshua Hoblitt <josh@hoblitt.com>
2018-12-27 13:40:47 -0700

    rm lsst/sqre specific var defaults

--- tf/variables.tf
+++ tf/variables.tf
@@ -14,12 +14,10 @@ variable "env_name" {
 
 variable "aws_zone_id" {
   description = "route53 Hosted Zone ID to manage DNS records in."
-  default     = "Z3TH0HRSNU67AM"
 }
 
 variable "domain_name" {
   description = "DNS domain name to use when creating route53 records."
-  default     = "lsst.codes"
 }
 
 variable "service_name" {
@@ -49,7 +47,6 @@ data "template_file" "fqdn" {
 
 variable "google_project" {
   description = "google cloud project ID"
-  default     = "plasma-geode-127520"
 }
 
 # Name of google cloud container cluster to deploy into
